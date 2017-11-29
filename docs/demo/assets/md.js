9ee3f336a7dcb79a3847e550156d547c537fed36
jacobshenning <jacobshenning@gmail.com>
2017-11-29 11:23:34 -0700

    Docs Update

---
+++ docs/demo/assets/md.js
@@ -0,0 +1,11 @@
+
+/* For Working with Markdown Files */
+
+function IsMarkdown(file) {
+    return file.substr(file.length - 3) == ".md";
+}
+
+function ReturnMarkdownInDiv(url) {
+    var MarkdownDiv = document.createElement('div');
+    
+}
